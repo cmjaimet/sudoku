8c32b20007c7acd6c3cf41a6f300c57398a83d68
Charles Jaimet <charles@calmseamedia.com>
2018-06-28 11:30:56 -0400

    Wrong approach but better
    Need to create a new Sudoku obj on Guess instead of Grid for proper recursion. That will be next

--- index.php
+++ index.php
@@ -3,7 +3,7 @@ require_once( 'classes/Sudoku.php' );
 
 require_once( 'classes/StringGrid.php' );
 
-for ( $x = 0; $x < 1; $x ++ ) {
+for ( $x = 0; $x < 10; $x ++ ) {
 	$grid = new StringGrid( 'data/grids_no.txt', $x );
 	$game = new Sudoku( $grid );
 	$game->play( 30 );
